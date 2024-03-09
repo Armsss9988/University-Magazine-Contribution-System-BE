fa2813f9f30f030ab25a724ec21892effc36068b
your name <pastover143@gmail.com>
2024-03-09 23:17:40 +0700

    fix SubmissionModel

--- models/Submission.js
+++ models/Submission.js
@@ -2,8 +2,10 @@
 const mongoose = require('mongoose');
 
 const submissionSchema = new mongoose.Schema({
+  
   title: { type: String, required: true },
   document_path: { type: String, required: true },
+  entry_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Entry', required: true },
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },
   closed: { type: Boolean, default: false },
