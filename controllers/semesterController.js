569566507111265844df2cd24abe2ec1a28f6048
Armsss9988 <96660862+Armsss9988@users.noreply.github.com>
2024-04-20 14:43:49 +0700

    change db

--- controllers/semesterController.js
+++ controllers/semesterController.js
@@ -98,7 +98,7 @@ const updateSemester = async (req, res) => {
         },
       },
     ]);
-    if ((existingSemester.length = 1  && !(existingSemester[0].academic_year == semester.academic_year))
+    if ((existingSemester.length = 1  && (existingSemester[0].academic_year != semester.academic_year))
           || existingSemester.length > 1) {
       return res
         .status(400)
